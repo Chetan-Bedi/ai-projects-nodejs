ca84516bf9ca99dacd1c4c4524631f81f8101e42
Chetan Bedi <chetanbedi141@gmail.com>
2025-02-26 12:42:22 +0530

    message

--- fundamentals/src/index.js
+++ fundamentals/src/index.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 const main = async () => {
-    const prompt = "write a haiku about ai"
+    const prompt = "write a haiku about ai?"
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
